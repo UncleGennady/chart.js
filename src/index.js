efda7b61b7f95b88d579f6a227832c9714d4a8a7
Aleksandr Protasenko <96978077+UncleGennady@users.noreply.github.com>
2022-08-25 19:32:37 +0300

    created obj config

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import users from './users_with_rating.js';
 console.log(users);
 
-const ctx = document.getElementById('myChart');
-const myChart = new Chart(ctx, {
+const data = {
+    labels: users.map(item => item.firstName + " " + item.lastName),
+    datasets: [{
+        label: 'rating group',
+        data: users.map(item => item.rating),
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+}
+const config = {
     type: 'bar',
-    data: {
-        labels: users.reduce((acc,item) =>{acc.push(item.firstName + " " + item.lastName);
-                            return acc;
-                            },[] ),
-        datasets: [{
-            label: 'rating group',
-            data: users.reduce((acc,item) =>{acc.push(item.rating);
-                return acc;
-            },[] ),
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+    data,
     options: {
         scales: {
             y: {
@@ -39,4 +35,7 @@ const myChart = new Chart(ctx, {
             }
         }
     }
-});
+}
+
+const ctx = document.getElementById('myChart');
+const myChart = new Chart(ctx, config);
